22a12a5b4ff35143d61b781ec330543c5b60d611
Zibri <Zibri@users.noreply.github.com>
2023-08-19 15:35:27 +0300

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -rf log
+rm -rf /var/run/log/mylog
 node server 127.0.0.1:8856 &
-ssh -o "StrictHostKeyChecking=no" -R 80:localhost:8856 nokey@localhost.run </dev/null &>log &
+ssh -o "StrictHostKeyChecking=no" -R 80:localhost:8856 nokey@localhost.run </dev/null &>/var/run/log/mylog &
 yarn &>/dev/null
 echo -n "Waiting for host"
 while [ "$(grep tunneled log)" == "" ]; do
@@ -9,7 +9,7 @@ while [ "$(grep tunneled log)" == "" ]; do
     sleep 1
 done
 echo
-hh=$(cat log|grep tunneled|cut -d " " -f 6|tail -1)
+hh=$(cat /var/run/log/mylog|grep tunneled|cut -d " " -f 6|tail -1)
 echo "alias exit='kill \$PPID'" >~/.bash_aliases
 echo alias url=\'\$qs\' >~/.bash_aliases
 node client $hh -L 8856
